22bdc7567c5be54c8ef76567731eb506d0fda2ef
d1ll0n <dillonkellar@protonmail.com>
2021-11-23 10:11:06 -0600

    Use fallthroughs in tests

--- test/IndexPoolRecovery.spec.ts
+++ test/IndexPoolRecovery.spec.ts
@@ -12,6 +12,7 @@ import {
 } from '../typechain';
 import { computeUniswapPairAddress, createSnapshot, deployContract, getContract, impersonate, sendEtherTo, withSigner } from './shared';
 import { ContractTransaction } from '@ethersproject/contracts';
+import { JsonRpcSigner } from '@ethersproject/providers'
 import {
   corePoolImplementationID,
   sigmaPoolImplementationID,
@@ -53,10 +54,13 @@ describe("IndexPoolRecovery", function() {
   let polygonERC20Predicate: ITokenPredicate;
   let coreControllerImplementation = '0x78b4f45B4A2Afa333C7Be1dbc7F2c9F056615327';
   let sigmaControllerImplementation = '0x4561784841DE5335922a9ad3E44aba69a162bA7A';
-  let corePoolImplementation = '0x669693A42B58E87b9e568bA2C6AdD607eb298d95';
+  let corePoolImplementation: string;
+  let sigmaPoolImplementation: string;
+  // let corePoolImplementation = '0x669693A42B58E87b9e568bA2C6AdD607eb298d95';
   const coreSellerImplementation = '0x2F0869D7AFd6638d2c83Fb2bfD79d5956D0cB952';
-  const sigmaPoolImplementation = '0x7B3B2B39CbdBddaDC13D8559D82c054b9C2fd5f3';
+  // const sigmaPoolImplementation = '0x7B3B2B39CbdBddaDC13D8559D82c054b9C2fd5f3';
   const sideChainDepositAmount = BigNumber.from(2).pow(128).sub(1);
+  let treasurySigner: JsonRpcSigner
   let weth: IERC20;
   let reset: () => Promise<void>
 
@@ -74,6 +78,10 @@ describe("IndexPoolRecovery", function() {
         }
       }))
     }));
+    const coreFallThrough = await deployContract('CoreFallThrough');
+    const sigmaFallThrough = await deployContract('SigmaFallThrough');
+    corePoolImplementation = coreFallThrough.address;
+    sigmaPoolImplementation = sigmaFallThrough.address;
     proxyManager = await getContract('0xD23DeDC599bD56767e42D48484d6Ca96ab01C115', 'IDelegateCallProxyManager')
     proxyManagerAccessControl = await getContract('0x3D4860d4b7952A3CAD3Accfada61463F15fc0D54', 'IProxyManagerAccessControl')
     recovery = await deployContract(
@@ -84,16 +92,20 @@ describe("IndexPoolRecovery", function() {
       sigmaPoolImplementation,
       wallet.address
     );
-    const { gasUsed } = await recovery.deployTransaction.wait()
-    console.log(`IndexPoolRecovery Gas Cost: ${gasUsed.toNumber()}`)
+    const { gasUsed: g1 } = await recovery.deployTransaction.wait()
+    const { gasUsed: g2 } = await coreFallThrough.deployTransaction.wait()
+    const { gasUsed: g3 } = await sigmaFallThrough.deployTransaction.wait()
+    console.log(`IndexPoolRecovery Gas Cost: ${g1.toNumber()}`)
+    console.log(`CoreFallThrough Gas Cost: ${g2.toNumber()}`)
+    console.log(`SigmaFallThrough Gas Cost: ${g3.toNumber()}`)
     polygonRootChainManager = await getContract('0xA0c68C638235ee32657e8f720a23ceC1bFc77C77', 'IRootChainManager');
     polygonERC20Predicate = await getContract('0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf', 'ITokenPredicate')
     weth = await getContract('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 'IERC20')
     await sendEtherTo(treasury)
     await sendEtherTo(recovery.address)
-    const signer = await impersonate(treasury)
-    recovery = recovery.connect(signer)
-    await proxyManagerAccessControl.connect(signer).transferOwnership(recovery.address);
+    treasurySigner = await impersonate(treasury)
+    recovery = recovery.connect(treasurySigner)
+    await proxyManagerAccessControl.connect(treasurySigner).transferOwnership(recovery.address);
     reset = await createSnapshot()
   })
 
@@ -336,17 +348,80 @@ describe("IndexPoolRecovery", function() {
       .to.emit(proxyManager, 'ManyToOne_ImplementationUpdated')
       .withArgs(coreSellerImplementationID, coreSellerImplementation)
     })
+
+    it('DEFI5 and CC10 can still access balanceOf', async () => {
+      const DEFI5 = await getContract(defi5, 'IERC20') as IERC20;
+      const CC10 = await getContract(cc10, 'IERC20') as IERC20;
+      const oldBalanceDEFI5 = await DEFI5.balanceOf(treasury)
+      const oldBalanceCC10 = await CC10.balanceOf(treasury)
+      expect(oldBalanceDEFI5).to.be.gt(0)
+      expect(oldBalanceCC10).to.be.gt(0)
+      await recovery.drainAndRepair()
+      expect(await DEFI5.balanceOf(treasury)).to.eq(oldBalanceDEFI5)
+      expect(await CC10.balanceOf(treasury)).to.eq(oldBalanceCC10)
+    })
+
+    it('DEFI5 and CC10 revert on other calls', async () => {
+      const DEFI5 = await getContract(defi5, 'IERC20') as IERC20;
+      const CC10 = await getContract(cc10, 'IERC20') as IERC20;
+      const oldBalanceDEFI5 = await DEFI5.balanceOf(treasury)
+      const oldBalanceCC10 = await CC10.balanceOf(treasury)
+      await recovery.drainAndRepair()
+      await expect(
+        DEFI5.connect(treasurySigner).transfer(wallet.address, oldBalanceDEFI5)
+      ).to.be.revertedWith('Contract disabled')
+      await expect(
+        CC10.connect(treasurySigner).transfer(wallet.address, oldBalanceCC10)
+      ).to.be.revertedWith('Contract disabled')
+    })
+
+    it('ORCL5 functions normally', async () => {
+      const ORCL5 = await getContract('0xD6cb2aDF47655B1bABdDc214d79257348CBC39A7', 'IERC20') as IERC20;
+      const oldBalanceORCL5 = await ORCL5.balanceOf(treasury);
+      await recovery.drainAndRepair();
+      await expect(ORCL5.connect(treasurySigner).transfer(wallet.address, oldBalanceORCL5))
+        .to.emit(ORCL5, 'Transfer')
+        .withArgs(treasury, wallet.address, oldBalanceORCL5)
+    })
+
+    it('FFF can still access balanceOf', async () => {
+      const FFF = await getContract(fff, 'IERC20') as IERC20;
+      const oldBalanceFFF = await FFF.balanceOf(treasury)
+      expect(oldBalanceFFF).to.be.gt(0)
+      await recovery.drainAndRepair()
+      expect(await FFF.balanceOf(treasury)).to.eq(oldBalanceFFF)
+    })
+
+    it('FFF reverts on other calls', async () => {
+      const FFF = await getContract(fff, 'IERC20') as IERC20;
+      const oldBalanceFFF = await FFF.balanceOf(treasury)
+      await recovery.drainAndRepair()
+      await expect(
+        FFF.connect(treasurySigner).transfer(wallet.address, oldBalanceFFF)
+      ).to.be.revertedWith('Contract disabled')
+    })
+
+    it('DEGEN functions normally', async () => {
+      const DEGEN = await getContract('0x126c121f99e1e211df2e5f8de2d96fa36647c855', 'IERC20') as IERC20;
+      const oldBalanceDEGEN = await DEGEN.balanceOf(treasury);
+      expect(oldBalanceDEGEN).to.be.gt(0)
+      await recovery.drainAndRepair();
+      await expect(DEGEN.connect(treasurySigner).transfer(wallet.address, oldBalanceDEGEN))
+        .to.emit(DEGEN, 'Transfer')
+        .withArgs(treasury, wallet.address, oldBalanceDEGEN)
+    })
+
+    it('NFTP functions normally', async () => {
+      const NFTP = await getContract('0x68bb81b3f67f7aab5fd1390ecb0b8e1a806f2465', 'IERC20') as IERC20;
+      const oldBalancNFTP = await NFTP.balanceOf(treasury);
+      await recovery.drainAndRepair();
+      await expect(NFTP.connect(treasurySigner).transfer(wallet.address, oldBalancNFTP))
+        .to.emit(NFTP, 'Transfer')
+        .withArgs(treasury, wallet.address, oldBalancNFTP)
+    })
   })
 
   describe('assumptions', () => {
-    it('Destroys only affected pools', async () => {
-      await recovery.drainAndRepair()
-      const codeAffected = await Promise.all(affectedPools.map(pool => ethers.provider.getCode(pool)));
-      const codeUnaffected = await Promise.all(unaffectedPools.map(pool => ethers.provider.getCode(pool)));
-      expect(codeAffected).to.deep.eq(['0x', '0x', '0x']);
-      expect(codeUnaffected).to.not.deep.eq(['0x', '0x', '0x']);
-    });
-
     it('Transfers no tokens in unaffected pools', async () => {
       await recovery.drainAndRepair()
       for (let i = 0; i < 3; i++) {
